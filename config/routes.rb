d5d31daacb607b3f7edeb6760ce4e0ddcbdf2b00
Guido <guido10md@gmail.com>
2021-05-07 14:48:19 -0300

    FC

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+require "api_constraints"
+
+Rails.application.routes.draw do
+  namespace :api, defaults: { format: :json } do
+    mount_devise_token_auth_for 'User', at: 'auth' #api/auth
+
+    scope module: :v1,
+      constraints: ApiConstraints.new(version: 1, default: true) do
+        resources :articles
+      end
+  end
+end
